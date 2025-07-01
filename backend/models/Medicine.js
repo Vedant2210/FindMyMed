0ef06f0ce6266da5e127666730c448fc563813f9
Vedant2210 <120133071+Vedant2210@users.noreply.github.com>
2025-07-01 20:42:37 +0530

    Files upload

--- backend/models/Medicine.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const medicineSchema = new mongoose.Schema({
-  name: { type: String, required: true, lowercase: true },
-  description: String
-});
-
-module.exports = mongoose.models.Medicine || mongoose.model('Medicine', medicineSchema);
-
